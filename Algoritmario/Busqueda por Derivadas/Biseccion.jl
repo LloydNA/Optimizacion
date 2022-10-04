2db56432be0f7770fb4bb3bc6e6c423cbfae9801
lloydnas <0239559@up.edu.mx>
2022-10-04 13:56:47 -0500

    Cambios 4 de Octubre

--- Algoritmario/Busqueda por Derivadas/Biseccion.jl
+++ Algoritmario/Busqueda por Derivadas/Biseccion.jl
@@ -1,19 +1,17 @@
 using Calculus
-using Makie
-using GLMakie
+using Plots
 
 f(x) = sin(x)
 
-image = lines(range(0, stop = 4*π, length = 200),f,color = :blue)
-
-function graph(initialize,first,a,b)
+function graph(initialize,a,b)
     if !initialize
-        if first
-            pop!(image.plots)
-            pop!(image.plots)
-        end
         scatter!(image,[a],[f(a)],label="a")
         scatter!(image,[b],[f(b)],label="b")
+
+        sleep(0.5)
+        Plots.series_list[end][:linealpha] = 0.0
+    else
+        plot!(f,0,2*π)
     end
 
     display(image)
@@ -24,19 +22,16 @@ function BSC()
     b = 2π
     error = 0.001
     needGraph = true
-    first = false
 
     if needGraph
-        graph(true,true,a,b)
+        graph(true,a,b)
     end
 
     while abs(a-b)>error
         alpha = (a+b)/2
 
         if needGraph
-            graph(false,first,a,b)
-            first = true
-            sleep(0.5)
+            graph(false,a,b)
         end
 
         if derivative(f,a)*derivative(f,alpha) <= 0
